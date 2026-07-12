286bca5a4e3cdc00767e2fc1fbea28ae58b4993b
Whocancode <aotzaaa3@gmail.com>
2026-07-12 18:43:12 +0700

    3011

---
+++ 3011/3011.py
@@ -0,0 +1,26 @@
+"""666"""
+
+
+def main():
+    """color"""
+    color1 = input()
+    color2 = input()
+
+    primary_colors = ["Red", "Yellow", "Blue"]
+
+    if color1 not in primary_colors or color2 not in primary_colors:
+        print("Error")
+
+    elif color1 == color2:
+        print(color1)
+
+    elif (color1 == "Red" and color2 == "Yellow") or (color1 == "Yellow" and color2 == "Red"):
+        print("Orange")
+    elif (color1 == "Red" and color2 == "Blue") or (color1 == "Blue" and color2 == "Red"):
+        print("Violet")
+    elif (color1 == "Yellow" and color2 == "Blue") or (color1 == "Blue" and color2 == "Yellow"):
+        print("Green")
+    else:
+        print("Error")
+
+main()
