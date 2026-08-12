d6fb5ae8ea970e00832c926b2691d8e66b1ac513
Whocancode <aotzaaa3@gmail.com>
2026-08-12 21:47:05 +0700

    3072

---
+++ 3072/AEIOU.py
@@ -0,0 +1,17 @@
+"""GOGOWA"""
+Cum = input().lower()
+a = Cum.count("a")
+e = Cum.count("e")
+i = Cum.count("i")
+o = Cum.count("o")
+u = Cum.count("u")
+if a > 0:
+    print(f"a : {a}")
+if e > 0:
+    print(f"e : {e}")
+if i > 0:
+    print(f"i : {i}")
+if o > 0:
+    print(f"o : {o}")
+if u > 0:
+    print(f"u : {u}")
